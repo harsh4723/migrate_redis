5c61b28ea1ef09d8a653bf3e93ef1ba0929807bc
harsh4723 <harsh4723@rediffmail.com>
2023-07-11 21:39:13 +0530

    feat: enable multithread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import redis
 import datetime
+import concurrent.futures
 
 print("Loading migration.....")
 
@@ -13,7 +14,8 @@ print("Len of keys",len(keys))
 
 start = datetime.datetime.now()
 
-for key in keys:
+
+def copy_key(key):
     value = source_redis.get(key)
     ttl = source_redis.ttl(key)
     if ttl >=0:
@@ -21,8 +23,17 @@ for key in keys:
     else:
         destination_redis.set(key, value)
 
+
+max_threads = 10
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+    futures = [executor.submit(copy_key, key) for key in keys]
+    concurrent.futures.wait(futures)
+
+source_redis.close()
+destination_redis.close()
+
 time_clocked = datetime.datetime.now() - start
 
 total_time = int(time_clocked.total_seconds())
-
 print("Copied Successfully total time taken",total_time)
