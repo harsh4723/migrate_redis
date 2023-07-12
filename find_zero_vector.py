3aa47d6e689c355ee2a2c7e2e8f0b7c9ef40c4d2
harsh4723 <harsh4723@rediffmail.com>
2023-07-12 20:01:08 +0530

    feat: use thread to compute fast

--- find_zero_vector.py
+++ find_zero_vector.py
@@ -1,18 +1,25 @@
 import redis
 import numpy as np
 import pickle
+import concurrent.futures
 
 source_redis = redis.Redis(host='ub-ec.prod.eu-west-2.infra', port=6379, db=1)
 
 keys = source_redis.keys('embeddings*')
 
-c = 0
-for key in keys:
+
+def process_key(key):
     value = source_redis.get(key)
     query_vector = pickle.loads(value)
-    if c%1000 == 0:
-        print(c)
     if np.max(query_vector[0]) == np.min(query_vector[0]) == 0.0:
-        c = c+1
+        return 1
+    else:
+        return 0
+
+c = 0
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    results = executor.map(process_key, keys)
+    c = sum(results)
 
 print(c)
